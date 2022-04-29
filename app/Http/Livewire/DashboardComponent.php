0f3b7fa99da00401fc7d6ddb710459875f0cd8a6
Dev Master <heroldtamko39@gmail.com>
2022-04-29 20:19:13 +0100

    update dashboard

--- app/Http/Livewire/DashboardComponent.php
+++ app/Http/Livewire/DashboardComponent.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\{Course, Department, Faculty, Room, Teacher, User};
 use Livewire\Component;
 
 class DashboardComponent extends Component
 {
     public function render()
     {
-        return view('livewire.dashboard-component');
+        //composer require asantibanez/livewire-charts
+        $users_count = User::withTrashed()->count();
+        // $users_count = User::all()->count();
+        $teachers_count = Teacher::all()->count();
+        $rooms_count = Room::all()->count();
+        $courses_count = Course::all()->count();
+        $departments_count = Department::all()->count();
+        $faculties_count = Faculty::all()->count();
+        return view('livewire.dashboard-component', compact('courses_count', 'departments_count', 'faculties_count','rooms_count' ,'teachers_count' ,'users_count'));
     }
 }
--- resources/views/livewire/dashboard-component.blade.php
+++ resources/views/livewire/dashboard-component.blade.php
@@ -3,89 +3,89 @@
         <div class="col-lg-3 col-6">
             <div class="small-box bg-info">
                 <div class="inner">
-                    <h3>150</h3>
+                    <h3>{{ $users_count }}</h3>
 
-                    <p>New Orders</p>
+                    <p>@lang("Users")</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="#" class="small-box-footer">@lang('More info') <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
 
         <div class="col-lg-3 col-6">
             <div class="small-box bg-success">
                 <div class="inner">
-                    <h3>150</h3>
+                    <h3>{{ $teachers_count }}</h3>
 
-                    <p>New Orders</p>
+                    <p>@lang('Teachers')</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="#" class="small-box-footer">@lang('More info') <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
 
         <div class="col-lg-3 col-6">
             <div class="small-box bg-warning">
                 <div class="inner">
-                    <h3>150</h3>
+                    <h3>{{ $rooms_count }}</h3>
 
-                    <p>New Orders</p>
+                    <p>@lang("Rooms")</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="#" class="small-box-footer">@lang('More info') <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
 
         <div class="col-lg-3 col-6">
             <div class="small-box bg-danger">
                 <div class="inner">
-                    <h3>150</h3>
+                    <h3>{{ $courses_count }}</h3>
 
-                    <p>New Orders</p>
+                    <p>@lang("Courses")</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="#" class="small-box-footer">@lang('More info') <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
 
         <div class="col-lg-3 col-6">
             <div class="small-box bg-primary">
                 <div class="inner">
-                    <h3>150</h3>
+                    <h3>{{ $departments_count }}</h3>
 
-                    <p>New Orders</p>
+                    <p>@lang('Departments')</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="#" class="small-box-footer">@lang('More info') <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
 
         <div class="col-lg-3 col-6">
             <div class="small-box bg-secondary">
                 <div class="inner">
-                    <h3>150</h3>
+                    <h3>{{ $faculties_count }}</h3>
 
-                    <p>New Orders</p>
+                    <p>@lang('Faculties')</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="#" class="small-box-footer">@lang('More info') <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
 
-        <div class="col-lg-3 col-6">
-            <div class="bg-white small-dark">
+        {{-- <div class="col-lg-3 col-6">
+            <div class="small-box bg-dark">
                 <div class="inner">
                     <h3>150</h3>
 
@@ -94,9 +94,9 @@
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="#" class="small-box-footer">@lang('More info') <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-        </div>
+        </div> --}}
 
     </div>
 </div>
